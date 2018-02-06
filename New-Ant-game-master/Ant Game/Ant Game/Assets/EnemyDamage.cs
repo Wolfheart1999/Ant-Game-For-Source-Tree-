6e027b799ddba8a4ddcfdbcfc2962a1eb5e866b9
SUSSEXDOWNS\463074 <bethsmith1999@yahoo.co.uk>
2018-02-06 16:56:55 +0000

    Enemy Code for the spikes!

---
+++ New-Ant-game-master/Ant Game/Ant Game/Assets/EnemyDamage.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour {
+
+    public float damage;
+    public float damageRate;
+    public float pushBackForce;
+
+    float nextDamage;
+
+	// Use this for initialization
+	void Start () {
+
+        nextDamage = 0f;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnTriggerStay2D(Collider2D Other)
+    {
+        if (Other.tag=="Player" && nextDamage<Time.time)
+        {
+            PlayerHealth thePlayerHealth = Other.gameObject.GetComponent<PlayerHealth>();
+            thePlayerHealth.addDamage(damage);
+            nextDamage = Time.time + damageRate;
+
+            pushback(Other.transform);
+        }
+    }
+    void pushback(Transform pushedObject){
+        Vector2 pushDirection = new Vector2(0, (pushedObject.position.y - transform.position.y)).normalized;
+        pushDirection *= pushBackForce;
+        Rigidbody2D PushRB = pushedObject.gameObject.GetComponent<Rigidbody2D> ();
+        PushRB.velocity = Vector2.zero;
+        PushRB.AddForce(pushDirection, ForceMode2D.Impulse);
+
+    }
+}
---
+++ New-Ant-game-master/Ant Game/Ant Game/Assets/Health.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public float fullHealth;
+    public GameObject DeathEffect;
+
+
+    float currentHealth;
+
+    Player ControlMovement;
+
+
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = fullHealth;
+
+        ControlMovement = GetComponent<Player>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void addDamage(float damage)
+    {
+        if (damage <= 0) return;
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            makeDead();
+        }
+    }
+    public void makeDead()
+    {
+        Instantiate(DeathEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
